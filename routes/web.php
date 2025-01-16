a3bfec578b3ee1d8a5f7559ef75b2da0e19c3e2e
Julia-Alcalde <jag0057@alu.medac.es>
2025-01-16 19:56:19 +0100

    cAMBIO

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TipoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcoDme');
+    return view('welcome');
 });
 
 Route::get('/muestras', [TipoController::class, 'mostrar']);

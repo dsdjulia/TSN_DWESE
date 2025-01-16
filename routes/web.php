79cdd8044ee2bd1acfd66ae5bc52f45d2861fd10
dsdjulia <jag0057@alu.medac.es>
2025-01-16 19:58:50 +0100

    Pruebas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TipoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('wedlcome');
+    return view('wedelcome');
 });
 
 Route::get('/muestras', [TipoController::class, 'mostrar']);

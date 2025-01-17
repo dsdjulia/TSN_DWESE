249c6c99f63a4233515d4f079b8508f9824df378
Javivv <jvv0014@alu.medac.es>
2025-01-17 19:57:29 +0100

    react

--- resources/views/formato.blade.php
+++ resources/views/formato.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuevo Informe</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 min-h-screen">
+    <div id="root"></div>
     <!-- Header -->
     <header class="text-white shadow-md py-4 bg-slate-500">
         <div class="container mx-auto flex justify-between items-center px-4">

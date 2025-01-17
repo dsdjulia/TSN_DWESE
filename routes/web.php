5fccf577269ab2a1ce1516f9ef37fbd32fa24786
Javivv <jvv0014@alu.medac.es>
2025-01-17 17:52:35 +0100

    rutas

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('formato');
 });
-
-Route::get('/muestras', [TipoController::class, 'mostrar']);
-Route::get('/muestras', [TipoController::class, 'mostrar']);
-
-Route::get('/',function (){
-    return view('index');
-});

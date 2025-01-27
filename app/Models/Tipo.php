22125cad664592d3c9e43d86c7028ee0decf6d5d
dsdjulia <jag0057@alu.medac.es>
2025-01-27 17:01:37 +0100

    arreglo de errores

--- app/Models/Tipo.php
+++ app/Models/Tipo.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Muestra;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tipo extends Model
 {

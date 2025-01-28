eec6c634b995faf78800e70d62d9cfecc71d2e58
dsdjulia <jag0057@alu.medac.es>
2025-01-28 18:33:37 +0100

    mg p11 (final en principio, modelos tambien)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,50 @@
 
 namespace App\Models;
 
-use App\Models\Muestra;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
-    
-    protected $table = "users"; 
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
+        'name',
         'email',
+        'password',
         'idSede',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
     public function sede(){
         return $this->belongsTo(Sede::class, 'idSede', 'id');

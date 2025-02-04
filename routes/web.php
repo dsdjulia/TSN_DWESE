af13f4d8d39ec3e67c4c0384b9e404aeac3420d3
cmr0025 <153531050+cmr0025@users.noreply.github.com>
2025-02-04 17:52:41 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,29 @@
 <?php
 
-use Inertia\Inertia;
-use App\Models\Muestra;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
-use App\Http\Controllers\SedeController;
-use App\Http\Controllers\TipoController;
-use App\Http\Controllers\MuestraController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\proyectoController;
+use App\Http\Controllers\TipoController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/',[proyectoController::class, 'index']) -> name('proyecto.index');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::get('/muestras',[proyectoController::class, 'muestras']) -> name('proyecto.muestras');
+Route::get('/referencia',[proyectoController::class, 'referencia']) -> name('proyecto.referencia');
+Route::get('/nuevaMuestra',[proyectoController::class, 'insercion']) -> name('proyecto.insercion');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/muestras', [MuestraController::class, 'mostrar'])->name('muestras');
-Route::get('/sedes', [SedeController::class, 'getJson'])->name('sedes');
 
 
 Route::get('/test', function () {

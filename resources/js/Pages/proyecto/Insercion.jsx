0fa7413fd7c28404c3ea256b51748ea8f1d328d1
Javivv <jvv0014@alu.medac.es>
2025-02-06 18:37:12 +0100

    cambios

--- resources/js/Pages/proyecto/Insercion.jsx
+++ resources/js/Pages/proyecto/Insercion.jsx
@@ -242,7 +242,6 @@ export default function Insercion({ auth }) {
         calidad: "",
         descripcionCalidad: "",
         interpretacion: "",
-        descripcion: "",
     });
 
     const handleData = (e) => {

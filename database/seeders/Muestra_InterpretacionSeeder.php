7f678c9a78f58ecf591f43c233110dfa08fe126c
dsdjulia <jag0057@alu.medac.es>
2025-02-17 15:56:31 +0100

    dompdf 1

--- database/seeders/Muestra_InterpretacionSeeder.php
+++ database/seeders/Muestra_InterpretacionSeeder.php
@@ -13,6 +13,35 @@ class Muestra_InterpretacionSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Muestra_Interpretacion::create([
+            'descripcion' => 'de la 1',
+            'idMuestra' => 1,
+            'idInterpretacion' => 1
+        ]);
+
+        Muestra_Interpretacion::create([
+            'descripcion' => 'de la 2',
+            'idMuestra' => 2,
+            'idInterpretacion' => 2
+        ]);
+
+        Muestra_Interpretacion::create([
+            'descripcion' => 'de la 3',
+            'idMuestra' => 3,
+            'idInterpretacion' => 3
+        ]);
+
+        Muestra_Interpretacion::create([
+            'descripcion' => 'de la 3.2',
+            'idMuestra' => 3,
+            'idInterpretacion' => 3
+        ]);
+
+
+        Muestra_Interpretacion::create([
+            'descripcion' => 'de la 5',
+            'idMuestra' => 5,
+            'idInterpretacion' => 5
+        ]);
     }
 }

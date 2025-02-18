f5e8dac50418e34e5dd312810ea17506073e8676
GiddonPk <jhg0015@alu.medac.es>
2025-02-18 21:41:09 +0100

    cambioslogin

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,7 +4,7 @@ import ApplicationLogo2 from '@/Components/ApplicationLogo2';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0  bg-[url('/public/fondo-login.webp')]">
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0  bg-[url('/public/fondo-login.webp')] bg-no-repeat bg-cover">
             <div>
                 <Link href="/">
                     <ApplicationLogo2 className="h-20 w-20 fill-current text-gray-500" />

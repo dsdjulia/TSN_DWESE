21b6f88f97e5a5956bb5bbc17624ee452cd23c5b
Javi Medac Pocho <jvv0014@alu.medac.es>
2025-02-18 16:33:29 +0100

    arreglo calidad

--- app/Http/Controllers/MuestraController.php
+++ app/Http/Controllers/MuestraController.php
@@ -64,7 +64,7 @@ class MuestraController extends Controller
             'idTipoNaturaleza' => intval($request->input('tipoNaturaleza')[0]),
             'idFormato' => intval($request->input('formato')[0]),
             'idCalidad' => intval($request->input('calidad')[0]),
-            'descripcionCalidad' => $request->input('descripcionCalidad'),
+            'descripcionCalidad' => $request->input('descripcionCalidad')[0],
             'organo' => $request->input('organo'),
             'fecha' => $request->input('fecha')[0], // Le pasaba un array con 1 solo valor
             

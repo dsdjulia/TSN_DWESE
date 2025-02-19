7a8323efa75ea1de73481ae8021f1c1298dd9a1f
dsdjulia <jag0057@alu.medac.es>
2025-02-19 18:10:22 +0100

    aaaa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        Vite::prefetch(concurrency: 3);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /* public function boot(): void
